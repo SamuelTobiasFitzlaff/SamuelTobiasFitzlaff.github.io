4dba780380effb4730ca16ac3dc7218bed549b54
SamuelTobiasFitzlaff <samueltobiasfitzlaff@gmail.com>
2023-07-09 21:42:11 -0300

    feat: folder structuring

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { Container } from "./styles";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import Head from "next/head";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Cris Jolli</title>
+      </Head>
+      <Container>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </Container>
+    </>
+  );
+};
+
+export { Layout };
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const pages: React.FC = () => {
+  return (
+    <div>
+      <h1>Pages</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat quod
+        cum possimus quasi corrupti labore nisi esse blanditiis non alias vel
+        similique quos, officia, quisquam impedit vero illum est autem?
+      </p>
+    </div>
+  );
+};
+
+export default pages;

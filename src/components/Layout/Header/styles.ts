704689d18baa001ee0ed4b966b94b831f23f2203
SamuelTobiasFitzlaff <samueltobiasfitzlaff@gmail.com>
2023-07-15 00:40:13 -0300

    feat: header styles

--- src/components/Layout/Header/styles.ts
+++ src/components/Layout/Header/styles.ts
@@ -13,4 +13,39 @@ export const HeaderContainer = styled.div`
   color: var(--text-secondary);
   backdrop-filter: blur(10px);
   height: 60px;
+
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  padding: 0.75rem 0.5rem;
+  gap: 0.5rem;
+`;
+
+const Button = styled.button`
+  cursor: pointer;
+  border: 1px solid transparent;
+  border-radius: 100vmax;
+  min-width: 6rem;
+  transition: filter 0.2s ease-in-out;
+
+  &:hover {
+    filter: brightness(1.1);
+  }
+`;
+
+export const ContactButton = styled(Button)`
+  background-color: var(--accent-secondary);
+  color: var(--text-secondary);
+  font-weight: 1000;
+`;
+
+export const RegularButton = styled(Button)`
+  background-color: var(--accent-tertiary);
+  color: var(--text-primary);
+  font-weight: 600;
 `;

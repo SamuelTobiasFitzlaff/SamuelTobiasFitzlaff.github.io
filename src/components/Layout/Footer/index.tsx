e82eb2f03900086670045cc343d350c52c071e6b
SamuelTobiasFitzlaff <samueltobiasfitzlaff@gmail.com>
2023-07-18 23:47:26 -0300

    feat: toggle theme button

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,10 +1,34 @@
-import { FooterContainer } from "./styles";
+import { useEffect, useState } from "react";
+import { FooterContainer, ThemeToggle } from "./styles";
+
+import { Sun, Moon } from "react-feather";
 
 export default function Footer() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  useEffect(() => {
+    setTheme(localStorage.getItem("theme") || "dark");
+  }, []);
+
   return (
     <FooterContainer>
-      <h1>Footer</h1>
-      <p>Lorem ipsum</p>
+      <div>
+        <h1>Footer</h1>
+        <p>Lorem ipsum</p>
+      </div>
+      <ThemeToggle onClick={toggleTheme}>
+        {theme === "dark" ? <Sun /> : <Moon />}
+      </ThemeToggle>
     </FooterContainer>
   );
 }
--- src/components/Layout/Footer/styles.ts
+++ src/components/Layout/Footer/styles.ts
@@ -1,7 +1,24 @@
+import { Button } from "@mui/base";
 import styled from "styled-components";
 
 export const FooterContainer = styled.div`
   background-color: rgb(var(--background-secondary), 0.788);
   color: rgb(var(--text-secondary));
-  padding: 20px;
+  padding: 1rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ThemeToggle = styled(Button)`
+  color: rgb(var(--text-primary));
+  background-color: rgb(var(--background-primary));
+  border: 1px solid transparent;
+
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 100vmax;
+
+  cursor: pointer;
 `;

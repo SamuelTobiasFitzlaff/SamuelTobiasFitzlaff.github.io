d41d4a445dac39d333be0c2784c11e87c21fb37f
SamuelTobiasFitzlaff <samueltobiasfitzlaff@gmail.com>
2023-07-25 00:04:44 -0300

    feat: pageContainer

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,12 +1,14 @@
+import { PageContainer } from "@/styles/styles";
+
 export default function projects() {
   return (
-    <div>
+    <PageContainer>
       <h1>Projects</h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat quod
         cum possimus quasi corrupti labore nisi esse blanditiis non alias vel
         similique quos, officia, quisquam impedit vero illum est autem?
       </p>
-    </div>
+    </PageContainer>
   );
 }

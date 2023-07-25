c960bb3ee4790f8a5b68faf099e8570c94b86196
SamuelTobiasFitzlaff <samueltobiasfitzlaff@gmail.com>
2023-07-25 15:23:21 -0300

    feat: add text to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,48 @@
-import { PageContainer } from "@/styles/styles";
-import Image from "next/image";
+import {
+  ContactButton,
+  HomePageContainer,
+  ProfilePhoto,
+} from "@/styles/styles";
 
 export default function pages() {
   return (
-    <PageContainer>
-      <Image
+    <HomePageContainer>
+      <ProfilePhoto
         src="/photos/profile.jpg"
         alt="Profile picture"
-        width={400}
-        height={400}
+        width={800}
+        height={800}
       />
-    </PageContainer>
+      <div>
+        <h2>🚧 This website is still under construction! 🚧</h2>
+        <br />
+        <h3>Welcome to my Portfolio!</h3>
+        <p>
+          I am a front-end developer with a passion for photography as a hobby.
+          In addition to photography, I have also worked on a few projects in
+          Blender. This website serves as a showcase of my interests and
+          creative pursuits.
+        </p>
+        <p>
+          Thank you for taking the time to explore my portfolio! If you have any
+          questions, collaboration opportunities, or just want to say hello,
+          feel free to reach out to me through the contact form below.
+        </p>
+
+        {/* <h2>Bem-vindo ao meu Portfólio!</h2>
+      <p>
+        Eu sou um desenvolvedor front-end e tenho a fotografia como meu hobby.
+        Além disso, também trabalhei em alguns projetos no Blender. Este website
+        foi criado como uma forma de apresentar meus interesses e criatividade.
+      </p>
+      <p>
+        Agradeço por dedicar seu tempo para explorar o meu portfólio. Caso tenha
+        alguma pergunta, oportunidade de colaboração ou apenas queira dizer oi,
+        sinta-se à vontade para entrar em contato comigo através do formulário
+        abaixo.
+      </p> */}
+        <ContactButton>Contact</ContactButton>
+      </div>
+    </HomePageContainer>
   );
 }
--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,13 +1,47 @@
 import { Button } from "@mui/base";
+import Image from "next/image";
 import styled from "styled-components";
 
 export const PageContainer = styled.div`
   padding: 2.5rem 1rem;
   margin: auto;
+`;
 
-  img {
-    border-radius: 50%;
+export const HomePageContainer = styled(PageContainer)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  gap: 1rem;
+
+  font-size: 1.125rem;
+  p {
+    max-width: 70ch;
+    margin-top: 1rem;
   }
+
+  @media screen and (max-width: 40rem) {
+    flex-direction: column;
+    align-items: center;
+    font-size: 1rem;
+  }
+`;
+
+export const ProfilePhoto = styled(Image)`
+  border-radius: 50%;
+  width: 35%;
+  height: 35%;
+  object-fit: cover;
+`;
+
+export const ContactButton = styled(Button)`
+  color: ${(props) => props.theme.accentSecondary};
+  font-size: 1.25rem;
+  padding: 0.5em 1em;
+  margin-top: 1rem;
+  background-color: transparent;
+  border: 1px solid ${(props) => props.theme.accentSecondary};
+  border-radius: 0.5em;
+  cursor: pointer;
 `;
 
 export const ThemeToggle = styled(Button)`

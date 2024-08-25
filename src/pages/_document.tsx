cf03bc196e358d3f44fc556b1fd9927a6de51e44
SamuelTobiasFitzlaff <samueltobiasfitzlaff@gmail.com>
2024-08-24 22:14:04 -0300

    add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ import Document, {
   NextScript,
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -43,6 +44,7 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
+        <GoogleAnalytics gaId="G-JQETSQW95G" />
       </Html>
     );
   }

decda3687e35e430ee22bd1623f298ead102b6cf
SamuelTobiasFitzlaff <samueltobiasfitzlaff@gmail.com>
2024-08-24 21:34:17 -0300

    add contact page

--- src/pages/projects/dev/index.tsx
+++ src/pages/projects/dev/index.tsx
@@ -3,12 +3,7 @@ import { PageContainer } from "@/styles/styles";
 export default function dev() {
   return (
     <PageContainer>
-      <h1>Dev</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat quod
-        cum possimus quasi corrupti labore nisi esse blanditiis non alias vel
-        similique quos, officia, quisquam impedit vero illum est autem?
-      </p>
+      <p>Coming soon :)</p>
     </PageContainer>
   );
 }
